b5b5eb36f8c4927b3c688bbf3e224dc88a4ef1d5
Szymon022 <szymon.kurpas00@gmail.com>
2021-05-04 16:14:05 +0200

    Created New game dialog UI

---
+++ app/src/main/java/com/szymon/thehangman/NewGameDialog.java
@@ -0,0 +1,10 @@
+package com.szymon.thehangman;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+
+public class NewGameDialog extends DialogFragment {
+
+}
